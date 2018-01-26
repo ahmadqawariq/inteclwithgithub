20e8ef0ce7d90979f0cf0377fa323139a6864acc
ahmadqawariq <ahmadqawariq.8@gmail.com>
2018-01-26 21:05:39 +0200

    hi

---
+++ src/inteclwithgithub/erordeer.java
@@ -0,0 +1,16 @@
+package inteclwithgithub;
+
+public class erordeer {
+public static int[] eorder(int a[]){
+		
+		return a;
+	}
+	public static void main(String[] args) {
+		
+int s[]= {1,21,1,12,12,33,2};
+eorder(s);
+
+	}
+
+
+}
